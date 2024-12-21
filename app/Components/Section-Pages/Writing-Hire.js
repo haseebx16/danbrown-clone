bc52e2f57cd6d2ef571c4fe704a3b055d2db5035
Anonymous <111380595+haseebx16@users.noreply.github.com>
2024-12-21 21:16:01 +0500

    book writing

--- app/Components/Section-Pages/Writing-Hire.js
+++ app/Components/Section-Pages/Writing-Hire.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { font2 } from '../font/poppins';
 
-const WritingHire = () => {
+const WritingHire = ({ head, text1, text2 }) => {
   return (
     <div
       className={`${font2.className} h-auto md:h-[39rem] bg-violet-950`}
@@ -19,23 +19,13 @@ const WritingHire = () => {
         {/* Right Content */}
         <div className="md:w-1/2 md:pl-8">
           <h1 className="text-3xl md:text-5xl text-white font-bold md:pt-4">
-            Why you should hire a ghostwriter for your book
+            {head}
           </h1>
           <p className="text-gray-400 md:text-lg mt-4">
-            Hiring a ghostwriter doesn’t mean you aren’t part of the production
-            process of your content. In today’s fast-paced world of tight
-            schedules and even tighter deadlines, hiring a ghostwriter can save
-            you the trouble (and the time) of getting your work done efficiently
-            and reliably. A ghostwriter can not only save you time and the work,
-            but they are also a good way to enhance your content as they can
-            suggest ideas or take creative liberties with your content.
+            {text1}
           </p>
           <p className="text-gray-400 md:text-lg mt-4">
-            At Dan Brown Academy, our ghostwriters take a dedicated ownership
-            of your project to ensure that they do the content complete justice.
-            Of course, the book is still yours by right and legality, but our
-            ghostwriters will put their heart and soul into your book to ensure
-            it surpasses your expectations.
+            {text2}
           </p>
         </div>
       </div>
