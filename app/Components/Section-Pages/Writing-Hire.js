5925d75f95ab72a783ccbb2c8034d159643544d9
Anonymous <111380595+haseebx16@users.noreply.github.com>
2024-12-21 22:15:35 +0500

    Marketing

--- app/Components/Section-Pages/Writing-Hire.js
+++ app/Components/Section-Pages/Writing-Hire.js
@@ -1,10 +1,23 @@
+"use client";
+
 import React from 'react';
 import { font2 } from '../font/poppins';
+import { useEffect } from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const WritingHire = ({ picsrc ,head, text1, text2 }) => {
+const WritingHire = ({ picsrc ,head, text1, text2,bg }) => {
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            once: true,
+        })
+    });
+
   return (
     <div
-      className={`${font2.className} h-auto md:h-[39rem] bg-violet-950`}
+      className={`${font2.className} h-auto md:h-[39rem] bg-${bg}`}
     >
       <div className="flex flex-col md:flex-row p-4 justify-center items-center">
         {/* Left Image */}
@@ -12,19 +25,20 @@ const WritingHire = ({ picsrc ,head, text1, text2 }) => {
           <img
             src={picsrc}
             alt="Hire Ghostwriter"
+            data-aos="flip-right"
             className="w-96 h-[21rem] mt-8 md:w-[35rem] md:h-[30rem]"
           />
         </div>
 
         {/* Right Content */}
         <div className="md:w-1/2 md:pl-8">
-          <h1 className="text-3xl md:text-5xl text-white font-bold md:pt-4">
+          <h1 data-aos="fade-down" className="text-3xl md:text-5xl text-gray-400 font-bold md:pt-4">
             {head}
           </h1>
-          <p className="text-gray-400 md:text-lg mt-4">
+          <p data-aos="fade-up-right" className="text-gray-400 md:text-lg mt-4">
             {text1}
           </p>
-          <p className="text-gray-400 md:text-lg mt-4">
+          <p data-aos="fade-up" className="text-gray-400 md:text-lg mt-4">
             {text2}
           </p>
         </div>
