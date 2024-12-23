7fb0f0f412c48fd0cbf830f4dae3a20419e5a65e
Abdul Haseeb Hamid <111380595+haseebx16@users.noreply.github.com>
2024-12-23 19:11:52 +0500

    publication

---
+++ app/publication/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { font2 } from '../Components/font/poppins'
+import MainSection from '../Components/Section-Pages/MainSection'
+
+const page = () => {
+  return (
+    <div className={`${font2.className} bg-gray-50 overflow-hidden`}>
+        <MainSection subHead={"INCONVENIENCE-FREE ONLINE"} mainHead={"Publication & Printing Services for Your Book"}
+            text={""}
+        />
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default page
