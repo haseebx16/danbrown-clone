e9b82a878ba6879b38f8a4bcb008b7857773211b
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-01-14 00:28:55 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,33 +1,19 @@
-"use client";
-
-import { useEffect } from 'react';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import localFont from "next/font/local";
 
 const timesNewRoman = localFont({
-  src: './times.ttf', // Path to the .ttf file in the public folder
-  variable: '--font-times-new-roman',
-  display: 'swap',
+  src: './times.ttf',
+  variable: '--font-times-new-roman',
+  display: 'swap',
 });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-// Apply the font to your components
 export default function Layout({ children }) {
-  return (
-    <html lang="en" className={`${timesNewRoman.variable} font-times-new-roman`}>
-      <body>
-        {children}
-      </body>
-    </html>
-  );
-}
+    return (
+      <html lang="en">
+        <body className={timesNewRoman.className}>
+          {children}
+        </body>
+      </html>
+    );
+  }
