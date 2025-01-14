4f1c35e0bffd704ade73d9f6e00c71d52eefb1c6
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-01-14 22:24:59 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,25 @@
-import { Geist, Geist_Mono } from "next/font/google";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import "./globals.css";
 import localFont from "next/font/local";
 
-const timesNewRoman = localFont({
-  src: './times.ttf',
-  variable: '--font-times-new-roman',
-  display: 'swap',
-});
-
+const timesNewRoman = 
+  localFont({
+      src: "./times.ttf",
+      variable: "--font-times-new-roman",
+      display: "swap",
+  });
+  
 export default function Layout({ children }) {
-    return (
-      <html lang="en">
-        <body className={timesNewRoman.className}>
-          {children}
-        </body>
-      </html>
-    );
-  }
+  
+  return (
+    <>
+      <html lang="en">
+        <body className={timesNewRoman?.className}>
+          {children}
+        </body>
+      </html>
+    </>
+  );
+}
